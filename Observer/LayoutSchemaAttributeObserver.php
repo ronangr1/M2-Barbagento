cf9783bcb1d9dcbf8954348a6882a3ca7b96e1aa
Ronan Guérin <r.guerin@webitinteractive.ca>
2021-05-30 22:10:16 +0200

    Fix ajax content updated

--- Observer/LayoutSchemaAttributeObserver.php
+++ Observer/LayoutSchemaAttributeObserver.php
@@ -9,6 +9,19 @@ use Magento\Framework\Event\ObserverInterface;
 
 class LayoutSchemaAttributeObserver implements ObserverInterface
 {
+    private \Magento\Framework\App\Request\Http $request;
+
+    /**
+     * LayoutSchemaAttributeObserver constructor.
+     * @param \Magento\Framework\App\Request\Http $request
+     */
+    public function __construct(
+        \Magento\Framework\App\Request\Http $request
+    )
+    {
+        $this->request = $request;
+    }
+
     public function execute(\Magento\Framework\Event\Observer $observer)
     {
         /** @var \Magento\Framework\View\Layout $layout */
@@ -18,7 +31,7 @@ class LayoutSchemaAttributeObserver implements ObserverInterface
             $output = $observer->getEvent()->getTransport()->getOutput();
             $output = str_replace(
                 '<main id="maincontent" class="page-main">',
-                '<main id="maincontent" class="page-main" data-barba="container" data-barba-namespace="' . $elementName . '">',
+                '<main id="maincontent" class="page-main" data-barba="container" data-barba-namespace="' . $this->request->getRouteName() . '">',
                 $output
             );
             $observer->getEvent()->getTransport()->setOutput($output);
